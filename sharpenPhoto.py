a481a10ffbff21b7241a9ba9a08cac05fb646df8
samuel <ge63wuv@mytum.de>
2023-02-26 13:28:30 +0100

    sharpening photos

---
+++ sharpenPhoto.py
@@ -0,0 +1,15 @@
+from PIL import Image, ImageEnhance, ImageFilter
+import os
+
+path = "./images" 
+pathOut = "./editedImages" 
+
+for filename in os.listdir(path):
+    img = Image.open(f"{path}/{filename}")
+
+    # Sharpening
+    edit = img.filter(ImageFilter.SHARPEN)
+
+    clean_name = os.path.splitext(filename)[0]
+
+    edit.save(f'.{pathOut}/{clean_name}_edited.jpg')
